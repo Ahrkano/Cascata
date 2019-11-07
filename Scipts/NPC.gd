86ea80b01416d2a1b404224edcf92bb7c715c25f
Ahrkano <36827623+Ahrkano@users.noreply.github.com>
2019-11-07 13:57:56 -0300

    dialog system ok

--- Scipts/NPC.gd
+++ Scipts/NPC.gd
@@ -1,8 +1,9 @@
 extends Node2D
 
+onready var dialogue = $DialogueUI
 var finished = false
 
 func _on_Area2D_body_entered(body):
-	if body.is_in_group("PLAYER"):
+	if body.is_in_group("PLAYER") and !finished:
 		finished = true
-		#DO STUFF
+		dialogue.StartDialogue()
