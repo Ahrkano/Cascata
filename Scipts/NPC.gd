f449c1c0e836a6093d4ca54253eeaf6f3a0ee989
Ahrkano <36827623+Ahrkano@users.noreply.github.com>
2019-11-07 16:58:01 -0300

    customized dialogue

--- Scipts/NPC.gd
+++ Scipts/NPC.gd
@@ -1,7 +1,13 @@
 extends Node2D
 
+export(String, FILE, "*.json") var npc_dialogue
 onready var dialogue = $DialogueUI
 var finished = false
+
+func _ready():
+	dialogue.dialog_path = npc_dialogue
+	dialogue.NPC_ready()
+
 
 func _on_Area2D_body_entered(body):
 	if body.is_in_group("PLAYER") and !finished:
